059aa7c372ff1c91c2c973674983458e9db3d055
Jan Drábek <me@jandrabek.cz>
2015-11-24 15:45:11 +0100

    Utils: enhance EOLPage search

--- library/tools/EOLPage.php
+++ library/tools/EOLPage.php
@@ -11,12 +11,16 @@ use Nette\Utils\Json;
  */
 class EOLPage extends EOLAPI
 {
-	const API_URL = 'http://eol.org/api/pages/1.0/__ID__.json?images=__IMAGES__&videos=0&sounds=0&maps=0&text=&iucn=false&subjects=overview&licenses=cc-by%2Ccc-by-nc%2Ccc-by-sa+cc-by-nc-sa%2Cpd&details=__DETAILS__&common_names=true&synonyms=false&references=false&vetted=1&cache_ttl=';
+	const VETTED_ONLY = 1;
+	const VETTED_UNREVIEWED = 2;
+
+	const API_URL = 'http://eol.org/api/pages/1.0/__ID__.json?images=__IMAGES__&videos=0&sounds=0&maps=0&text=&iucn=false&subjects=overview&licenses=cc-by%2Ccc-by-nc%2Ccc-by-sa+cc-by-nc-sa%2Cpd&details=__DETAILS__&common_names=true&synonyms=false&references=false&vetted=__VETTED__&cache_ttl=';
 
 	private $images;
 	private $details;
+	private $vetted;
 
-	public function __construct($images = 0, $details = FALSE, $key = NULL)
+	public function __construct($images = 0, $details = FALSE, $vetted = self::VETTED_ONLY, $key = NULL)
 	{
 		parent::__construct($key);
 		$this->images = $images;
@@ -29,6 +33,7 @@ class EOLPage extends EOLAPI
 		$temp = str_replace('__ID__', urlencode($id), self::API_URL);
 		$temp = str_replace('__IMAGES__', urlencode($this->images), $temp);
 		$temp = str_replace('__DETAILS__', urlencode($this->details), $temp);
+		$temp = str_replace('__VETTED__', urlencode($this->vetted), $temp);
 		return $temp;
 	}
 
