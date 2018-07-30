509c7ac66a3c3d81eba69a407cd216db78d539fc
Jan Drábek <jan.drabek@trigama.eu>
2018-07-30 08:29:35 +0200

    Add BioLibNames extractor

---
+++ library/tools/BioLibNames.php
@@ -0,0 +1,27 @@
+<?php
+namespace NatureQuizzer\Tools;
+
+
+class BioLibNames
+{
+	use CurlDownloader;
+
+	const API_URL = "http://www.biolib.cz/cz/formsearch/?action=execute&searcharea=1&string=__QUERY__";
+
+	private function prepareUrl($query)
+	{
+		return str_replace('__QUERY__', urlencode($query), self::API_URL);
+	}
+
+	public function getData($query)
+	{
+		$result = $this->fetchByCurl($this->prepareUrl($query));
+		if ($result) {
+			preg_match('#<h1>.*?<strong><em>(.*?)</em>.*?<\/strong>.*?<\/h1>#su', $result, $matches);
+			if (isset($matches[1])) {
+				return $matches[1];
+			}
+		}
+		return NULL;
+	}
+}
